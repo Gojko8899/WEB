7d40ce9f5c05362085f5fa25ed69a18370471508
BIT student <classroom@bgit.rs>
2018-11-05 18:15:53 +0100

    event_js_complete

--- Event_JS/main.js
+++ Event_JS/main.js
@@ -1,25 +1,14 @@
-// var $button = document.createElement("button");
-// $button.setAttribute("type", "button");
-// $button.setAttribute("value", "Toggle");
-// var body = document.querySelector("body");
-
-// body.appendChild($button);
-
 var body = document.querySelector("body");
-
+var timeer;
 function togglebackground() {
   body.classList.toggle("active");
-  setInterval(togglebackground, 1000);
+
+  if (timeer === undefined) {
+    timeer = setInterval(togglebackground, 1000);
+  }
 }
 
 function turnOff() {
-  clearInterval(setInterval);
+  clearInterval(timeer);
+  timeer = undefined;
 }
-
-// var hasRunningInterva;
-// var setInterval;
-// function automatic() {
-//   if (!hasRunningInterval) {
-//     hasRunningInterval = setInterval(toggle, 1000);
-//   }
-// }
